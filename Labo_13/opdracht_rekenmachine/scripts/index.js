b6856b355c6ed4313fb9549fd67a883199072204
Falco-12 <falco.soetaert@student.vives.be>
2025-02-07 07:16:54 +0100

    symbolen rekenmachine aangepast

--- Labo_13/opdracht_rekenmachine/scripts/index.js
+++ Labo_13/opdracht_rekenmachine/scripts/index.js
@@ -33,7 +33,7 @@ const aftrekken = () => {
 	let g2=parseInt(txtRechts.value, 10);
 	let resultaat = g1-g2;
 
-	let resultaatTekst=g1+" + "+g2+" = "+resultaat;
+	let resultaatTekst=g1+" - "+g2+" = "+resultaat;
 	txtOutput.innerHTML=resultaatTekst;
 }
 
@@ -46,7 +46,7 @@ const vermenigvuldigen = () => {
 	let g2=parseInt(txtRechts.value, 10);
 	let resultaat = g1*g2;
 
-	let resultaatTekst=g1+" + "+g2+" = "+resultaat;
+	let resultaatTekst=g1+" x "+g2+" = "+resultaat;
 	txtOutput.innerHTML=resultaatTekst;
 }
 
@@ -59,7 +59,7 @@ const delen = () => {
 	let g2=parseInt(txtRechts.value, 10);
 	let resultaat = g1/g2;
 
-	let resultaatTekst=g1+" + "+g2+" = "+resultaat;
+	let resultaatTekst=g1+" : "+g2+" = "+resultaat;
 	txtOutput.innerHTML=resultaatTekst;
 }
  
